3845672b878ba721d74251de16e12463b8471fba
mbenson025 <mbenson025@gmail.com>
2022-05-09 00:02:17 -0500

    added more variables to JS, api still not working

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 //document ready function
-$(function (){
-
+// $(function (){
+// });
 
 //global variables
 var apiKey = 'b6a631faf48ec36736fa91299da2f0a2';
 var cityWeather;
 var recentHistory;
+var lat;
+var lon;
+var temp;
+var wind;
+var humidity;
+var uv;
+
+var currentTime = moment().hour();
+// console.log(currentTime);
+
 
 let data = [];
 
-
-function corsError() {
-var queryURL = `http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}`;
+getWeather();
+function getWeather() {
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?appid={apiKey}&units=imperial`;
+//imperial for fahrenheit(still doesn't work)
 
 
 $.ajax({
   type: 'GET',
   url: queryURL,
 
-  }).then(function() {
-        console.log();
-
+  success: function(data) {
+    console.log('success');
+  }
 });
 
 }
 
 
-// eventListeners-----
-$('searchBtn').on('click', cityWeather);
 
-});
+
+
+
+// eventListeners-----
+// $('searchBtn').on('click', getWeather);
